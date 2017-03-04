d5736898d55976d53653284696b34e987dfdfb3d
Wang Guan <momocraft@gmail.com>
2017-03-05 05:48:35 +0900

    parse selector

--- src/main/java/org/jsoup/parser/SaxHtmlElementsMatcher.java
+++ src/main/java/org/jsoup/parser/SaxHtmlElementsMatcher.java
@@ -1,10 +1,10 @@
 package org.jsoup.parser;
 
 import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Element;
 
 import java.util.ArrayList;
-import java.util.Locale;
-import java.util.regex.Pattern;
+import java.util.List;
 
 /**
  * Match and build HTML Element(s) from a stream of Sax events
@@ -25,29 +25,61 @@ public class SaxHtmlElementsMatcher extends SaxEventListener.NopSaxEventListener
      * of tag, classes, id is supplied
      */
     static class ElementsPath extends SaxEventListener.NopSaxEventListener {
+
+        private final ElementQualifier[] qualifiers;
+        private final ArrayList<Element> matched = new ArrayList<Element>(4);
+
+        // depth in a complete DOM
+        private int currentDepth = 0;
+        private int matchedDepth = 0;
+
         public ElementsPath(String selector) {
-            // TODO parse selector into an array of PathSegment
-            // TODO keep a stack of tags by far
             TokenQueue tq = new TokenQueue(selector);
+
+            ArrayList<ElementQualifier> pathSelector = new ArrayList<ElementQualifier>();
 
             while (!tq.isEmpty()) {
                 tq.consumeWhitespace();
                 if (tq.consume() != '>') {
-                    Validate.fail(String.format(Locale.ENGLISH, "Expected at '%s'", tq.remainder()));
+                    throw new SaxHtmlMatcherException("Expected > at %s", tq.remainder());
                 }
+                tq.consumeWhitespace();
 
-                ArrayList<String> tagQualifierStrings = null;
-                ArrayList<String> idQualifierStrings = null;
-                ArrayList<String> classQualifierStrings = null;
+                ArrayList<ElementQualifier> thisLevel = new ArrayList<ElementQualifier>();
 
                 while (true) {
-
+                    if (tq.matchChomp("#")) {
+                        String id = tq.consumeCssIdentifier();
+                        Validate.notEmpty(id);
+                        thisLevel.add(new ElementQualifier.IdIs(id));
+                    } else if (tq.matchChomp(".")) {
+                        String klass = tq.consumeCssIdentifier();
+                        Validate.notEmpty(klass);
+                        thisLevel.add(new ElementQualifier.HaveClass(klass));
+                    } else if (tq.matchesWord()) {
+                        String tag = tq.consumeTagName();
+                        Validate.notEmpty(tag);
+                        thisLevel.add(new ElementQualifier.TagIs(tag));
+                    } else {
+                        if (thisLevel.size() == 0) {
+                            throw new SaxHtmlMatcherException("expected selector before %s", tq.remainder());
+                        }
+                        pathSelector.add(new ElementQualifier.And(thisLevel));
+                        tq.consumeWhitespace();
+                        break;
+                    }
                 }
             }
+
+            if (pathSelector.size() == 0) {
+                throw new SaxHtmlMatcherException("expected selector");
+            }
+
+            this.qualifiers = pathSelector.toArray(ElementQualifier.EmptyQualifierArray);
         }
 
-        private void parse() {
-
+        Element[] getMatched() {
+            return matched.toArray(new Element[0]);
         }
 
         /**
@@ -61,21 +93,61 @@ public class SaxHtmlElementsMatcher extends SaxEventListener.NopSaxEventListener
         public boolean isMatching() {
             return false;
         }
+
+        @Override
+        public void onStartTag(Token.StartTag token) {
+            super.onStartTag(token);
+        }
+
+        @Override
+        public void onEndTag(Token.EndTag token) {
+            super.onEndTag(token);
+        }
+
+        @Override
+        public void onDocType(Token.Doctype token) {
+            super.onDocType(token);
+        }
+
+        @Override
+        public void onComment(Token.Comment token) {
+            super.onComment(token);
+        }
+
+        @Override
+        public void onCharacter(Token.Character token) {
+            super.onCharacter(token);
+        }
+
+        @Override
+        public void onEOF(Token.EOF token) {
+            super.onEOF(token);
+        }
+
     }
+
 
     /**
      * Part of NodePath
      */
     abstract static class ElementQualifier {
+
+        private static final ElementQualifier[] EmptyQualifierArray = new ElementQualifier[0];
+
         abstract boolean match(Token.StartTag startTag);
 
         /**
          * Combined qualifiers for one element
          */
-        abstract static class And extends ElementQualifier {
+        static class And extends ElementQualifier {
+
             private final ElementQualifier[] qualifiers;
 
-            private And(ElementQualifier... qualifiers) {
+            And(List<ElementQualifier> qualifierList) {
+                this(qualifierList.toArray(EmptyQualifierArray));
+            }
+
+            And(ElementQualifier... qualifiers) {
                 this.qualifiers = qualifiers;
                 Validate.notNull(qualifiers);
                 Validate.isTrue(qualifiers.length > 0, "qualifier array cannot be null");
@@ -91,9 +163,9 @@ public class SaxHtmlElementsMatcher extends SaxEventListener.NopSaxEventListener
         }
 
         /**
-         * tag
+         * tag (default to be case insensitive)
          */
-        abstract static class TagIs extends ElementQualifier {
+        static class TagIs extends ElementQualifier {
             private final String tag;
             private final boolean caseSensitive;
 
@@ -118,7 +190,7 @@ public class SaxHtmlElementsMatcher extends SaxEventListener.NopSaxEventListener
         /**
          * id equality (case sensitive)
          */
-        abstract static class IdIs extends ElementQualifier {
+        static class IdIs extends ElementQualifier {
             private final String id;
 
             IdIs(String id) {
@@ -135,7 +207,7 @@ public class SaxHtmlElementsMatcher extends SaxEventListener.NopSaxEventListener
         /**
          * have class (case sensitive)
          */
-        abstract static class HaveClass extends ElementQualifier {
+        static class HaveClass extends ElementQualifier {
             private final String classname;
 
             HaveClass(String classname) {
